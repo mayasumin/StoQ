cddb5eae25b6a31f3800d9c497708aec41ee980b
mayasumin <yasmin.sampaio09@aluno.ifce.edu.br>
2025-07-03 02:47:27 -0300

    client: inicio da tela de notas de entrada

--- client/src/app/pages/nota-fiscal/nota-fiscal-list/nota-fiscal-list.ts
+++ client/src/app/pages/nota-fiscal/nota-fiscal-list/nota-fiscal-list.ts
@@ -1,14 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SHARED_TABLE_IMPORTS } from '../../../shared/shared-imports/shared-table-imports';
+import { MatTableDataSource } from '@angular/material/table';
+import { NotaFiscalHeader, NotaFiscalService } from '../../../services/nota-fiscal';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nota-fiscal-list',
   imports: [
-    RouterModule
+    CommonModule,
+    RouterModule,
+    ...SHARED_TABLE_IMPORTS
   ],
   templateUrl: './nota-fiscal-list.html',
   styleUrl: './nota-fiscal-list.scss'
 })
-export class NotaFiscalList {
+export class NotaFiscalList implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<NotaFiscalHeader>([]);
+  displayedColumns = ['id', 'fornecedor', 'numero', 'serie', 'dataEmissao', 'detalhes'];
+
+  error?: string;
+  loading = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private nfService: NotaFiscalService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.loadPage();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadPage(): void {
+    this.loading = true;
+    this.nfService.listAll().subscribe({
+      next: list => {
+        this.dataSource.data = list;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Erro ao carregar produtos';
+        this.loading = false;
+      }
+    });
+  }
+
+  getIdCurto(id: string): string {
+    return `${id.slice(0, 4)}...${id.slice(-4)}`;
+  }
 
 }
